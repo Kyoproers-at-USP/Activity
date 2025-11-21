3650ae5aace529e6aed19bd517ad601a58354616
johnnymancer <141833424+johnnymancer@users.noreply.github.com>
2025-11-21 18:44:49 +0900

    Add files via upload

--- AtCoder/ABC401~500/ABC432_A.cpp
+++ AtCoder/ABC401~500/ABC432_A.cpp
@@ -1,24 +1,12 @@
-/*アルゴリズムと工夫点(Permute to Maximize/CPU: 1 ms Memory: 3596 KB  Length: 396 B)
-A,B,Cを値の降順にソートして繋げ合わせる．
-*/
-#include<iostream>
-#include<algorithm>
-#include<cassert>
-#define rep(i,n) for(i=0;i<(int)(n);i++)
+#include <bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-typedef unsigned long long ull;
-
-string ipt = "";
 
 int main(){
-    int n = 3;
-    while(n--){
-        string tmp;
-        cin >> tmp;
-        ipt += tmp;
-    }
-    sort(ipt.rbegin(),ipt.rend());
-    cout << ipt << endl;
-    return 0;
+  vector<int> a(3);
+  for(int i = 0 ; i < 3 ; i++){
+    cin >> a.at(i);
+  }
+  
+  sort(a.begin(),a.end());
+  cout << a.at(2) << a.at(1) << a.at(0) << endl;
 }
