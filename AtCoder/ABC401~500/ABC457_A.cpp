45adbe99acf4f51b4cbbad954d8ccd32099a7b91
johnnymancer <141833424+johnnymancer@users.noreply.github.com>
2026-05-15 14:47:12 +0900

    feat(ABC457):Add answer codes of ABC457

--- AtCoder/ABC401~500/ABC457_A.cpp
+++ AtCoder/ABC401~500/ABC457_A.cpp
@@ -1,23 +1,20 @@
-/*アルゴリズムと工夫点(Array/CPU: 2 ms Memory: 3868 KB  Length: 373 B)
-入力データを一次元配列で保持し，指定された要素を出力すればよい．
-*/
-#include<iostream>
-#include<vector>
-#include<cassert>
-#define rep(i, n) for(i = 0;i < (int)(n);i++)
+// AtCoder template
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
-typedef long long ll;
-typedef unsigned long long ull;
 
-int n;
+#define rep(i,n) for(int i = 0 ; i < (int)(n) ; i++)
 
 int main(){
-    int i;
-    scanf("%d", &n);
-    
+    int n ;
+    cin >> n;
     vector<int> a(n);
-    rep(i, n)scanf("%d", &a[i]);
-    scanf("%d", &i);
-    printf("%d\n", a[i - 1]);
-    return 0;
+    rep(i,n){
+        cin >> a.at(i);
+    }
+    int x ;
+    cin >> x;
+
+    cout << a.at(x-1) << endl;
 }
--- AtCoder/ABC401~500/ABC457_B.cpp
+++ AtCoder/ABC401~500/ABC457_B.cpp
@@ -1,30 +1,26 @@
-/*アルゴリズムと工夫点(Arrays/CPU: 28 ms Memory: 14384 KB  Length: 509 B)
-2次元配列で入力を保持し，指定された要素を出力すればよい．
-可変長配列で持てば，メモリ効率は良い．
-*/
-#include<iostream>
-#include<vector>
-#include<cassert>
-#define rep(i, n) for(i = 0;i < (int)(n);i++)
+// AtCoder template
+#include <iostream>
+#include <string>
+#include <vector>
 using namespace std;
-typedef long long ll;
-typedef unsigned long long ull;
 
-int n, x, y;
+#define rep(i,n) for(int i = 0 ; i < (int)(n) ; i++)
 
 int main(){
-    int i, j;
-    scanf("%d", &n);
-    
-    vector<vector<int> > a(n);
-    rep(i, n){
-        int l;
-        scanf("%d",&l);
-        a[i] = vector<int>(l);
-        rep(j, l)scanf("%d", &a[i][j]);
+    int n ; cin >> n;
+    vector<vector<int>> a(n,vector<int>(0));
+
+    rep(i,n){
+        int l ;cin >> l;
+
+        rep(j,l){
+            int push_num; cin >> push_num;
+            a.at(i).push_back(push_num);
+        }
     }
 
-    scanf("%d%d", &x, &y);
-    printf("%d\n", a[x - 1][y - 1]);
-    return 0;
+    int x , y;
+    cin >> x >> y;
+
+    cout << a.at(x-1).at(y-1) << endl;
 }
