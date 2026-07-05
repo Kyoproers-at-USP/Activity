6e63946c8b1e4476e33efa0d15a3434391dc0ca5
johnnymancer <141833424+johnnymancer@users.noreply.github.com>
2026-07-05 11:00:01 +0900

    feat:johnnymancerのGit勉強会の演習分のコード共有

---
+++ AtCoder/practice66/AtCoder Beginners Selection_PracticeA_johnnymancer.cpp
@@ -0,0 +1,15 @@
+// AtCoder template
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+#define rep(i,n) for(int i = 0 ; i < (int)(n) ; i++)
+
+int main(){
+    int a,b,c;
+    string s;
+
+    cin >> a >> b >> c >> s;
+    cout << a+b+c << " " << s;
+}
