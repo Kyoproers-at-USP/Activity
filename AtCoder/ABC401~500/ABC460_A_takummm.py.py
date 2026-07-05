1dad650f4e7f42989019b1ecea07871bc1fc1352
takumi <taku3sub423@gmail.com>
2026-07-05 11:08:21 +0900

    コードを修正しました

--- AtCoder/ABC401~500/ABC460_A_takummm.py.py
+++ AtCoder/ABC401~500/ABC460_A_takummm.py.py
@@ -1,8 +1,7 @@
-n= 0
-N, M = map(int, input().split())
-while M != 0:
-    x = N % M
-    M = x
-    n = n + 1
-print(n)  
-  
+cnt= 0
+n, m = map(int, input().split())
+while m != 0:
+    x = n % m
+    m = x
+    cnt = cnt + 1
+print(cnt)   
