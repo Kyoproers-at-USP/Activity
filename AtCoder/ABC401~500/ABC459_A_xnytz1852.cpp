c4c139e11f50a6daae8534c2bfd24e6c698d3405
xnytz1852 <287625278+xnytz1852@users.noreply.github.com>
2026-07-05 12:00:47 +0900

    xnytz1852のABC459のA問題の回答の修正

--- AtCoder/ABC401~500/ABC459_A_xnytz1852.cpp
+++ AtCoder/ABC401~500/ABC459_A_xnytz1852.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 
 int main() {
-  string str1;
+  string str1 = "HelloWorld";
   int x;
   cin >> x;
-  str1 = "HelloWorld";
 
-  for(int i=0;i<10;i++){
+  for(int i = 0;i < str1.length();i++){
     if(i==x-1)continue;
     cout << str1.at(i);
   }
