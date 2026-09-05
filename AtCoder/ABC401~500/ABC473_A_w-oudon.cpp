939dd032074fcc3fc7ade6e3f4246bd0d1f7028f
中嶋海吏 <275111804+w-oudon@users.noreply.github.com>
2026-09-05 14:36:54 +0900

    修正後のABC473_A_w-oudon.cpp

--- AtCoder/ABC401~500/ABC473_A_w-oudon.cpp
+++ AtCoder/ABC401~500/ABC473_A_w-oudon.cpp
@@ -8,11 +8,11 @@ int main()
     int sum = 0;
     
     for(int i = 0; i < N; i++)
-    cin >> A[i];
+        cin >> A[i];
     
     for(int i = N/2; i < N; i++){
-    sum += A[i];
-}
+        sum += A[i];
+    }
 
-cout << sum << endl;
+    cout << sum << endl;
 }
